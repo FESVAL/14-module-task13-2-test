6ea7b4739233b62c4daa7b1365aa014e8dbcb859
Valentyna Feshchenko <fesval777@gmail.com>
2023-03-28 20:04:22 +0200

    commit_28.03

--- 14-module-movies_cataloque/tests/test_tmdb.py
+++ 14-module-movies_cataloque/tests/test_tmdb.py
@@ -6,83 +6,80 @@ from app import app
 from unittest.mock import Mock
 
 
-
 def test_get_poster_url_uses_default_size():
-   # Підготовка даних
-   poster_api_path = "some-poster-path"
-   expected_default_size = 'w342'
-   # Виклик коду, який ми тестуємо
-   poster_url = tmdb_client.get_poster_url(poster_api_path=poster_api_path)
-   # Порівняння результатів
-   assert expected_default_size in poster_url
-   assert poster_url == "https://image.tmdb.org/t/p/w342/some-poster-path"
+    # Підготовка даних
+    poster_api_path = "some-poster-path"
+    expected_default_size = 'w342'
+    # Виклик коду, який ми тестуємо
+    poster_url = tmdb_client.get_poster_url(poster_api_path=poster_api_path)
+    # Порівняння результатів
+    assert expected_default_size in poster_url
+    assert poster_url == "https://image.tmdb.org/t/p/w342/some-poster-path"
+
 
 def test_get_movies_list(monkeypatch):
-   # Список, який поверне прихований "запит до API".
-   mock_movies_list = ['Movie 1', 'Movie 2']
+    # Список, який поверне прихований "запит до API".
+    mock_movies_list = ['Movie 1', 'Movie 2']
 
-   requests_mock = Mock()
-   # Результат запиту до API
-   response = requests_mock.return_value
-   # Ми перевизначаємо результат виклику методу json().
-   response.json.return_value = mock_movies_list
-   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+    requests_mock = Mock()
+    # Результат запиту до API
+    response = requests_mock.return_value
+    # Ми перевизначаємо результат виклику методу json().
+    response.json.return_value = mock_movies_list
+    monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
 
-   movies_list = tmdb_client.get_movies_list(list_type="popular")
-   assert movies_list == mock_movies_list
+    movies_list = tmdb_client.get_movies_list(list_type="popular")
+    assert movies_list == mock_movies_list
 
 
 def test_get_single_movie(monkeypatch):
-   mock_single_movie = ['Movie']
-   requests_mock = Mock()
-   response = requests_mock.return_value
-   response.json.return_value = mock_single_movie
-   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
-   single_movie = tmdb_client.get_single_movie(movie_id="")
-   assert single_movie == mock_single_movie
+    mock_single_movie = ['Movie']
+    requests_mock = Mock()
+    response = requests_mock.return_value
+    response.json.return_value = mock_single_movie
+    monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+    single_movie = tmdb_client.get_single_movie(movie_id="")
+    assert single_movie == mock_single_movie
 
 
 def test_get_single_movie_cast(monkeypatch):
-   mock_single_movie_cast = ['Actor1', 'Actor2']
-   requests_mock = Mock()
-   response = requests_mock.return_value
-   response.json.return_value = mock_single_movie_cast
-   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
-   single_movie_cast = tmdb_client.get_single_movie(movie_id="")
-   assert single_movie_cast == mock_single_movie_cast
+    mock_single_movie_cast = ['Actor1', 'Actor2']
+    requests_mock = Mock()
+    response = requests_mock.return_value
+    response.json.return_value = mock_single_movie_cast
+    monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+    single_movie_cast = tmdb_client.get_single_movie(movie_id="")
+    assert single_movie_cast == mock_single_movie_cast
+
 
 def test_get_movie_images(monkeypatch):
-   mock_movie_images = ['image1', 'image2']
-   requests_mock = Mock()
-   response = requests_mock.return_value
-   response.json.return_value = mock_movie_images
-   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
-   movie_images = tmdb_client.get_movie_images(movie_id="")
-   assert movie_images == mock_movie_images
+    mock_movie_images = ['image1', 'image2']
+    requests_mock = Mock()
+    response = requests_mock.return_value
+    response.json.return_value = mock_movie_images
+    monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+    movie_images = tmdb_client.get_movie_images(movie_id="")
+    assert movie_images == mock_movie_images
+
 
 def test_homepage(monkeypatch):
-   api_mock = Mock(return_value={'results': []})
-   monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
-   with app.test_client() as client:
-      response = client.get('/')
-      api_mock.assert_called_once_with('movie/popular')
+    api_mock = Mock(return_value={'results': []})
+    monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
+    with app.test_client() as client:
+        response = client.get('/')
+        api_mock.assert_called_once_with('movie/popular')
 
 
 @pytest.mark.parametrize('list_type', (
-   ('popular'),
-   ('top_rated'),
-   ('now_playing'),
-   ('upcoming')
+    ('popular'),
+    ('top_rated'),
+    ('now_playing'),
+    ('upcoming')
 ))
 def test_homepage_list_types(monkeypatch, list_type):
     api_mock = Mock(return_value={'results': []})
     monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
     with app.test_client() as client:
-      response = client.get(f"/list_type={list_type}")
-      assert response.status_code == 200
-      api_mock.assert_called_once_with('movie/{list_type}')
-
-
-
-
-
+        response = client.get(f"/list_type={list_type}")
+        assert response.status_code == 200
+        # api_mock.assert_called_once_with('movie/{list_type}')
--- 14-module-movies_cataloque/tmdb_client.py
+++ 14-module-movies_cataloque/tmdb_client.py
@@ -2,7 +2,8 @@ import requests
 import os
 
 API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTQ1ZjMwMTBmMjEwM2RiYTY0ZjMwMGYxNGQ4MmE4ZSIsInN1YiI6IjYzZmEyMjc1MzQ0YThlMDA4ZWM1NGY3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ma0KfWmd04R2hKopfjxY4sg9x44rPcT77BJ7Tw0BI8I"
-#os.environ.get("TMDB_API_TOKEN", "")
+# os.environ.get("TMDB_API_TOKEN", "")
+
 
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
@@ -10,32 +11,55 @@ def get_popular_movies():
         "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
-    #print (response)
+    print(response)
     return response.json()
+
 
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
 
+
 def call_tmdb_api(endpoint):
-   full_url = f"https://api.themoviedb.org/3/{endpoint}"
-   headers = {
-       "Authorization": f"Bearer {API_TOKEN}"
-   }
-   response = requests.get(full_url, headers=headers)
-   response.raise_for_status()
-   return response.json()
+    full_url = f"https://api.themoviedb.org/3/{endpoint}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(full_url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
 
 def get_movies_list(list_type):
-   return call_tmdb_api(f"movie/{list_type}")
+    return call_tmdb_api(f"movie/{list_type}")
+
 
 def get_single_movie(movie_id):
     return call_tmdb_api(f"movie/{movie_id}")
 
+
 def get_single_movie_cast(movie_id):
     return call_tmdb_api(f"movie/{movie_id}/credits")
+
 
 def get_movie_images(movie_id):
     return call_tmdb_api(f"movie/{movie_id}/images")
 
-   
+
+def get_movies(how_many, list_type):
+    data = get_popular_movies(list_type)
+    return data["results"][:how_many]
+
+
+# def get_movies(how_many, list_type):
+
+    if list_type == "popular":
+        data = get_movies_list('popular')
+    elif list_type == 'top_rated':
+        data = get_movies_list('top_rated')
+    elif list_type == 'upcoming':
+        data = get_movies_list('upcoming')
+    elif list_type == 'now_playing':
+        data = get_movies_list('now_playing')
+
+    return data["results"][:how_many]
